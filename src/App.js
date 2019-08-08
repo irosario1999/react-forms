c7a7907abb173b27dbf47e8991fb33236513c55d
Seyfat Khamidov <seyfat123@gmail.com>
2019-08-08 09:40:59 -0400

    latest changes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,46 @@
-import React, { Component } from "react";
-import Form from "./componets/Form";
 
-export default class App extends Component {
-  state = {
-    firstName: "",
-    lastName: "",
-    email: ""
-  };
 
-  getFormValues = (e, name) => {
-    this.setState({
-      [name]: e.target.value
-    }, () => console.log(this.state));
-  };
 
-  getAllFormData = e => {
-    e.preventDefault();
-    const {firstName, lastName} = this.state
-    alert(`Hello ${firstName} ${lastName}`)
-  };
 
-  render() {
-    return (
-      <>
-        <h1>Hello Guys!</h1>
-        <Form
-          getFormValues={this.getFormValues}
-          getAllFormData={this.getAllFormData}
-        />
-      </>
-    );
-  }
-}
+
+
+
+
+
+
+
+// import React, { Component } from "react";
+// import Form from "./componets/Form";
+// import './App.css'
+// export default class App extends Component {
+//   state = {
+//     firstName: "",
+//     lastName: "",
+//     email: ""
+//   };
+
+//   getFormValues = (e, name) => {
+//     this.setState({
+//       [name]: e.target.value
+//     }, () => console.log(this.state));
+//   };
+
+//   getAllFormData = e => {
+//     e.preventDefault();
+//     const {firstName, lastName} = this.state
+//     alert(`Hello ${firstName} ${lastName}`)
+//   };
+
+//   render() {
+//     return (
+//       <>
+//         <h1>Hello Guys!</h1>
+//         {/* <a href="https://www.google.com" target="_blank" rel="noopener noreferrer" onClick={this.getAllFormData}>click</a> */}
+//         <Form
+//           getFormValues={this.getFormValues}
+//           getAllFormData={this.getAllFormData}
+//         />
+//       </>
+//     );
+//   }
+// }
